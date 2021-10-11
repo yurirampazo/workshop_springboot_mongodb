08e4a24dbd7787ac49e992deab8c7608aab471fe
yurirampazo <ymrampazo@gmail.com>
2021-10-11 00:39:32 -0300

    First Commit: Generated Project

---
+++ src/main/java/com/yurirampazo/workshop_springboot_mongodb/WorkshopSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.yurirampazo.workshop_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurirampazo/workshop_springboot_mongodb/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurirampazo.workshop_springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
